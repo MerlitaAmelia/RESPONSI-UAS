c10c2f043a25c048136622122f1090329068ddad
MerlitaAmelia <61940838+MerlitaAmelia@users.noreply.github.com>
2020-07-07 10:40:11 +0700

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
 
+namespace ProjectCustomer
+{ 
+  public class Customer 
+  { 
+    // PERINTAH: lengkapi property class customer, sesuai petunjuk soal 
+    public string KodeCustomer { get; set; } 
+    public string NamaCustomer { get; set; } 
+    public int TotalPiutang { get; set; } 
+    public void Print() 
+    { 
+        Console.WriteLine("{0}, {1}, {2}", KodeCustomer, NamaCustomer, TotalPiutang); 
+     } 
+  }
+}
